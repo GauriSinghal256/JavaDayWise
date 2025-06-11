a01e9dec31a9e3f9035fdd928a0ce59592887039
GauriSinghal256 <singhalg818@gmail.com>
2025-06-11 19:42:17 +0530

    linked-list

---
+++ LinkedListttt/linkedListt.java
@@ -0,0 +1,216 @@
+
+
+public class linkedListt{
+
+    // Node class
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        } 
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+
+    // function to add in the linkedlist at first position
+    // Here time complexity is O(1)
+    public void addFirst(int data){
+
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add last in a linkedlist 
+    // time complexity is O(1)
+    public void addLast(int data){
+
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // print a linkedlist
+    public void print(){
+        if(head == null){
+            System.out.println("Linked List is empty");
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    // Add in the middle of the linkedlistt
+    public void add(int idx , int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i=0;
+        while(i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // remove in a linkedlist - remove first 
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("Linkedlist is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size =0;
+            return val;
+        }
+        int val = head.data;
+        size--;
+        head = head.next;
+        return val;
+    }
+
+    // remove in a linkedlist - remove last
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("Linkedlist is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size =0;
+            return val;
+        }
+        Node prev = head;
+        for(int i=0 ; i<size-2 ; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    // remove in a linkedlist - middle
+    public int remove(int idx){
+        if(idx == 0){
+            removeFirst();
+        }else if(idx == size){
+            removeLast();
+        }
+        Node prev = head;
+        for(int i=0 ; i<idx-1 ; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = prev.next.next;
+        size--;
+        return val;
+    }
+
+
+    // Search in a linked list iterative 
+    public int search(int key){
+        Node temp = head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    // search recursive in a linked list 
+    public int helper(Node head , int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next , key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+
+    }
+    public  int recSearch(int key){
+        return helper(head , key);
+    }
+
+    // reverse a linked list 
+    // just to remember it - there are 3 variables and 4 steps
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    // Find and remove the nth node from end
+    
+
+
+
+    
+    public static void main(String args[]){
+        linkedListt ll = new linkedListt();
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
+        // ll.head.next.next = new Node(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2, 8);
+        ll.removeFirst();
+        ll.removeLast();
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.remove(2);
+        ll.print();
+        // System.out.println(ll.search(4));
+        // System.out.println(ll.recSearch(4));
+        ll.reverse();
+        ll.print();
+
+
+
+    }
+}
--- recursion/rec.java
+++ recursion/rec.java
@@ -52,7 +52,103 @@ public class rec {
     }
 }
 
+// factorial of a number using recursion
+    public static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
 
+    // compute the sum of digits 
+    public static int digitSum(int n){
+        if(n<=0){
+            return 0;
+        }
+        int rem = n%10;
+        return rem+digitSum(n/10);
+    }
+
+    // compute the product of digits
+     public static int digitProd(int n){
+        if(n<=0){
+            return 1;
+        }
+        int rem = n%10;
+        return rem*digitSum(n/10);
+    }
+
+    // Reverse the number using recursion  just print 
+    public static void reverse(int n ){
+        if(n<=0){
+            return;
+        }
+
+        System.out.print(n%10);
+        reverse(n/10);
+    }
+    
+    // Reverse the number using recursion 
+    public static int rev1(int n , int length){
+        if(n<=0){
+            return 0;
+        }
+
+        return (n%10)*(int)Math.pow(10,length-1) + rev1(n/10 , length-1);
+    }
+
+    // Reverse 2nd method 
+    static int sum = 0;
+    static void rev2(int n){
+        if(n == 0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10+rem;
+        rev2(n/10);
+    }
+
+    // reverse 3rd method 
+    public static int rev3(int n){
+
+        int digits = (int)(Math.log10(n))+1;
+        return helper(n , digits);
+    }
+    
+    public static int helper(int n , int digits){
+        if(n%10 == n){
+            return n;
+        }
+        int rem = n%10;
+        return rem * (int) Math.pow(10 , digits-1) + helper(n/10 , digits-1);
+    }
+
+    // check if the number is palindrom or not 
+    public static boolean palindrom(int n){
+        return n == rev3(n);
+    }
+
+    // Count the number of zeroes 
+    public static int count(int n){
+        int c = 0;
+        return helpercount(n , c);
+    }
+
+    public static int helpercount(int n, int c){
+        if(n==0){
+            return c;
+        }
+        int rem = n%10;
+        if(rem == 0){
+            return helpercount(n/10 , c+1);
+        }else{
+            return helpercount(n/10 , c);
+        }
+    }
+
+
+
+   
 
 
     public static void main(String[] args) {
@@ -66,8 +162,21 @@ public class rec {
         // System.out.println(fibo(5));
 
         // binary search using recursion 
-        int arr[] = {1 , 2, 3 , 4 , 5 , 8, 10};
-        System.out.println(binarySearch(arr, 11, 0, 6));
+        // int arr[] = {1 , 2, 3 , 4 , 5 , 8, 10};
+        // System.out.println(binarySearch(arr, 11, 0, 6));
+
+        // factorial 
+        // System.out.println(factorial(5));
+        // System.out.println(digitSum(12345));
+        // System.out.println(digitProd(123454));
+        // reverse(12345);
+        // System.out.println(rev1(123456 , 6));
+        // rev2(678980);
+        // System.out.println(sum);
+        //    System.out.println(rev3(897654));
+        // System.out.println(palindrom(1232109));
+        System.out.println(count(12340));
+        
 
 
 
