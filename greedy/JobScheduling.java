45d867a6e5869cbb266c3dca57216ae4031dd697
GauriSinghal256 <singhalg818@gmail.com>
2025-06-17 19:11:01 +0530

    greedy

--- greedy/JobScheduling.java
+++ greedy/JobScheduling.java
@@ -1,11 +1,47 @@
+import java.util.*;
 
 public class JobScheduling {
 
     // Given an array of jobs where every job has a deadline and profit if the job is finished before the deadline .
     // It is also given that every job takes a single unit of time, so the minimum possible deadline for any job is 1.
     // maximise the total profit if only one job can be scheduled at a time
-   public static void main(String[] args) {
-    
+     static class Job{
+        int deadline;
+        int profit;
+        int id;
+
+        public Job(int i,int d, int p){
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+     }
+
+    public static void main(String[] args) {
+     int jobsInfo[][] = {{4,20} , {1,10} , {1,40} , {1,30}};
+
+     ArrayList<Job> jobs = new ArrayList<>();
+     for(int i=0 ; i<jobsInfo.length ; i++){
+        jobs.add(new Job(i , jobsInfo[i][0] ,jobsInfo[i][1] ));
+     }
+     Collections.sort(jobs, (obj1,obj2)-> obj2.profit-obj1.profit); // descending order of profit
+     
+     ArrayList<Integer> seq = new ArrayList<>();
+     int time = 0;
+     for(int i=0 ; i<jobs.size() ; i++){
+      Job curr = jobs.get(i);
+      if(curr.deadline>time){
+         seq.add(curr.id);
+         time++;
+      }
+     }
+
+   //   print sequence
+   System.out.println("max jobs =" + seq.size());
+   for(int i=0 ; i<seq.size() ; i++){
+      System.out.print(seq.get(i)+" ");
+    }
+   System.out.println();
    }
 }
 
